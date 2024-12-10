61e2d4f1cd67d26ebef75cfa6d26a40ebd2eca20
Yash-Panchal-521 <yp06446@gmail.com>
2024-12-10 21:20:44 +0530

    Live Video Streaming Application

---
+++ server/src/controllers/auth/postLogin.js
@@ -0,0 +1,29 @@
+import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const postLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        process.env.TOKEN_KEY,
+        { expiresIn: "8h" }
+      );
+      return res.status(200).json({
+        userDetails: {
+          email: user.email,
+          token,
+          username: user.username,
+        },
+      });
+    }
+    return res.status(400).send("Invalid credentials. Please try again");
+  } catch (err) {
+    return res.status(500).send("Something went wrong. Please try again.");
+  }
+};
---
+++ server/src/controllers/auth/postRegister.js
@@ -0,0 +1,31 @@
+import User from "../../models/User.js";
+import Channel from "../../models/Channel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const postRegister = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const userExists = await User.exists({ email });
+    if (userExists) return res.status(409).send("E-mail already in use");
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newChannel = await Channel.create({});
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      channel: newChannel._id,
+    });
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.TOKEN_KEY,
+      { expiresIn: "8h" }
+    );
+    return res.status(201).json({
+      userDetails: { email: user.email, username, token },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Error occured. Please try again");
+  }
+};
